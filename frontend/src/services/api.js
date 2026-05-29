ef76d48f9cb9af52c62eac4fd27b818a41593344
rizqirifahamarulloh <rizqirifah.a@gmail.com>
2026-05-30 04:12:00 +0700

    fix: sederhanakan baris env url menjadi satu baris

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,10 +1,7 @@
 // frontend/src/services/api.js
 import axios from 'axios'
 
-// Memastikan Vercel mengambil URL Online, jika tidak ada baru pakai lokal
-export const API_URL = import.meta.env.VITE_API_URL 
-  ? import.meta.env.VITE_API_URL 
-  : 'http://localhost:8000/api';
+export const API_URL = import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : 'http://localhost:8000/api';
 
 const api = axios.create({
   baseURL: API_URL,
